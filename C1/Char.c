bc6563ed81a27a28b205b04027f73bc73d201674
unknown <fkemboik@gmail.com>
2022-07-25 15:38:14 +0300

    C++

---
+++ C1/Char.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	printf("Character: %c, Integer: %d\n", 'F', 'F');
+	
+}
+
---
+++ C1/p6.c
@@ -0,0 +1,10 @@
+/** read and print the first 3 characters in the data */
+#include <stdio.h>
+int main()
+{
+	printf("Type some data and press'Enter'\n");
+	for (int h =1; h<= 6; h++){
+		char ch = getchar();
+		printf("Character %d is %c \n", h, ch);
+	}
+}
